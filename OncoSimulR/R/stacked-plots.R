a6835bcf48185e6c650d22ed2e140586e090dbe0
Daniel Cerdan <dcerdanv@gmail.com>
2018-12-20 12:01:37 +0100

    Add pseudocode to fish plot fn

--- OncoSimulR/R/stacked-plots.R
+++ OncoSimulR/R/stacked-plots.R
@@ -146,83 +146,17 @@ plot.fish2 <- function(
 ){
   print("Fish plot")
   
-  if(sum(y < 0, na.rm = TRUE) > 0) stop("y cannot contain negative numbers")
+  # population <- y
+  # num_of_clones <- x[["NumClones"]]
+  # time_points =  x
   
-  if(is.null(border)) border <- par("fg")
-  border <- as.vector(matrix(border, nrow=ncol(y), ncol=1))
-  col <- as.vector(matrix(col, nrow=ncol(y), ncol=1))
-  lwd <- as.vector(matrix(lwd, nrow=ncol(y), ncol=1))
+  # data <- as.vector(t(population[,2:(num_of_clones + 1)]))
+  # dimnames <- list(cloneid = c(1:num_of_clones), time= time_points)
+  # mat <- matrix(data, ncol = length(time_points), nrow = num_of_clones, dimnames = dimnames)
+  # pop_df <- as.data.frame(as.table(mat))
+  # colnames(pop_df) <- c("Generation", "Identity", "Population")
   
-  if(order.method == "max") {
-    ord <- order(apply(y, 2, which.max))
-    y <- y[, ord]
-    col <- col[ord]
-    border <- border[ord]
-  }
-  
-  if(order.method == "first") {
-    ord <- order(apply(y, 2, function(x) min(which(x>0), na.rm = TRUE)))
-    y <- y[, ord]
-    col <- col[ord]
-    border <- border[ord]
-  }
-  
-  bottom.old <- x*0
-  top.old <- x*0
-  polys <- vector(mode="list", ncol(y))
-  for(i in seq(polys)){
-    if(i %% 2 == 1){ #if odd
-      top.new <- top.old + y[,i]
-      polys[[i]] <- list(x=c(x, rev(x)), y=c(top.old, rev(top.new)))
-      top.old <- top.new
-    }
-    if(i %% 2 == 0){ #if even
-      bottom.new <- bottom.old - y[,i]
-      polys[[i]] <- list(x=c(x, rev(x)), y=c(bottom.old, rev(bottom.new)))
-      bottom.old <- bottom.new
-    }
-  }
-  
-  ylim.tmp <- range(sapply(polys,
-                           function(x) range(x$y, na.rm=TRUE)), na.rm=TRUE)
-  outer.lims <- sapply(polys,
-                       function(r) rev(r$y[(length(r$y)/2+1):length(r$y)]))
-  mid <- apply(outer.lims, 1,
-               function(r) mean(c(max(r, na.rm=TRUE),
-                                  min(r, na.rm=TRUE)), na.rm=TRUE))
-  
-  ## center and wiggle
-  if(center) {
-    g0 <- -mid + runif(length(x),
-                       min=frac.rand*ylim.tmp[1],
-                       max=frac.rand*ylim.tmp[2])
-  } else {
-    g0 <- runif(length(x),
-                min=frac.rand*ylim.tmp[1],
-                max=frac.rand*ylim.tmp[2])
-  }
-  fit <- smooth.spline(g0 ~ x, spar=spar)
-  
-  for(i in seq(polys)){
-    polys[[i]]$y <- polys[[i]]$y + c(fit$y, rev(fit$y))
-  }
-  
-  if(is.null(ylim)) ylim <- range(sapply(polys,
-                                         function(x) range(x$y, na.rm=TRUE)),
-                                  na.rm=TRUE)
-  if(grepl("x", log))
-    axes <- FALSE
-  else
-    axes <- TRUE
-  plot(x,y[,1], ylab=ylab, xlab=xlab, ylim=ylim, t="n", axes = axes, ...)
-  for(i in seq(polys)){
-    polygon(polys[[i]], border=border[i], col=col[i], lwd=lwd[i])
-  }
-  if(!axes) {
-    ## yes, we only allow transformation of x axis
-    relabelLogaxis(1)
-    axis(2)
-  }
+  # print(pop_df)
 }
 
 
