552c1f5a6284101c3dca437794096a3fa3ee16f0
juanjovazpar <juanjovazpar@gmail.com>
2019-01-08 13:01:06 +0100

    Temp commit

--- OncoSimulR/R/OncoSimulR.R
+++ OncoSimulR/R/OncoSimulR.R
@@ -1307,9 +1307,7 @@ plotClonesSt <- function(z,
                        ylim = ylim,
                        xlim = xlim,
                        ...)
-        } else if (type == "fish") {
-          plot.fish2(simulation = z)
-        }
+        } 
         if(show == "drivers") {
             if(legend.ncols == "auto") {
                 if(length(cll$colorsLegend$Drivers) > 6) legend.ncols <- 2
@@ -1341,6 +1339,9 @@ plotClonesSt <- function(z,
                    col = cll$colors,
                    legend = ldrv,
                    ncol = legend.ncols)
+        }
+        if (type == "fish") {
+          plot.fish2(simulation = z)
         }
     }
 }
